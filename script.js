d192f0b50192260632dc07d3c14c419e579112a8
LeoKu11 <98027759+LeoKu11@users.noreply.github.com>
2022-05-31 16:26:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,18 @@ class GoodsList {
   }
 }
 
+class BasketGoods{
+    items=[];
+    fetchGoods=(callback=()=>{}){
+        service(GET_BASKET_GOODS_ITEMS, (data)=>{
+             this.items=data;
+             callback();
+        {);
 
+
+}
+const basketGoods=new BasketGoods();
+basketGoods.fetchGoods();
 
 const goodsList = new GoodsList();
 goodsList.fetchGoods(() => {
