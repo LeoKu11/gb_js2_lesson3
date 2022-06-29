eb33021c846778c1b60b324fd38275a8eb67c7f2
LK <lab4students@yandex.ru>
2022-06-29 12:47:11 +0500

    lesson7

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
-const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
-const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
+const BASE_URL = 'http://localhost:8000/';
+const GET_GOODS_ITEMS = `${BASE_URL}goods`
+// const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
 
 function service(url) {
   return fetch(url)
   .then((res) => res.json())
 }
+
 
 function init() {
   const CustomButton = Vue.component('custom-button', {
@@ -21,18 +22,51 @@ function init() {
          basketGoodsItems: []
       }
     },
+    
+    template: `
+      <div class="fixed-area">
+         <div class="basket-card">
+            <div class="basket-card__header">
+               <h1 class="basket-card__header__title">basket card</h1>
+               <div class="basket-card__header__delete-icon"
+                  v-on:click="$emit('closeclick')"
+               ></div>
+            </div>
+            <div class="basket-card__content">
+               content
+            </div>
+         </div>
+      </div>
+    `,
+    mounted() {
+    
+    }
   })
   
-
+  const goodsItem = Vue.component('goods-item', {
+    props: [
+       'item'
+    ],
+    template: `
+      <div class="goods-item">
+         <h3>{{ item.product_name }}</h3>
+         <p>{{ item.price }}</p>
+      </div>
+    `
+  })
   
   const app = new Vue({
     el: '#root',
     data: {
       items: [],
       filteredItems: [],
-      search: ''
+      search: '',
+      cardIsVision: false
     },
     methods: {
+      setVisionCard() {
+        this.cardIsVision = !this.cardIsVision
+      },
       fetchGoods() {
         service(GET_GOODS_ITEMS).then((data) => {
           this.items = data;
@@ -58,4 +92,3 @@ function init() {
   })
 }
 window.onload = init
-  
